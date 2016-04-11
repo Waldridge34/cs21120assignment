b6894caf8044bb6d775cfc19184083b69c8cb8a1
CommanderWillum <commanderwillum@gmail.com>
2016-04-11 08:30:13 +0100

    the main work but need to get it working

--- src/cs21120/assignment/Tree.java
+++ src/cs21120/assignment/Tree.java
@@ -4,44 +4,15 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Tree implements IBinaryTree{
+public class Tree{
 	public ArrayList<String> teams = new ArrayList<String>();
+	private BNode root;
+	BNode pointer = root;
 	
 	
 	public void main() throws FileNotFoundException{
-		
-	
-		
-		
 		startComp();
-		
 	}
-	
-	@Override
-	public IBinaryTree getLeft() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IBinaryTree getRight() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getPlayer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getScore() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	
 	
 	public void startComp() throws FileNotFoundException{
 		CompetitionManager CompManger = new CompetitionManager(null);
@@ -50,47 +21,114 @@ public class Tree implements IBinaryTree{
 	
 	public void playTree(ArrayList<String> teams){
 		
-		int noOfTeams = teams.size();
-		BNode pointer;		
-		pointer = root;
-		if (noOfTeams == 1) {
-			root.name = teams.get(1);
-		} 
-		else{ 
-			
-		}
-		
-		
-		double x = Math.floor(Math.log(noOfTeams));
-		for (int y = 0; y < teams.size(); y++){
-			for (int i = 0; i < x; i++){
-				pointer.left.perent = pointer;
-				pointer.left = pointer;
-			
-			}
-			if(pointer.left.name != "not played"){
-				pointer.left.name = teams.get(y);
-			}
-			else if(pointer.right.name != "not played"){
-				pointer.right.name = teams.get(y);
-			}
-			else 
-		}
-		
 	}
-	
 	
 	public void setTree (ArrayList<String> teams){
+		int numberOfTeams = teams.size();
+		double levelDown = Math.floor((Math.log(numberOfTeams)/Math.log(2)));
+		int expand = (int) (numberOfTeams - Math.pow(levelDown, 2));
+		int down = 0;
+		int ex = 0;
+		int t = 0;
+		
+		while(root.name!="not played"){
+			if (root.left.name == "not played" && root.right.name == "not played"){
+				pointer.name = "not played";
+				break;
+			}
+				
+			if (pointer.left.name == "not played" && pointer.right.name == "not played"){
+				pointer.name = "not played";
+				if (pointer != root){
+					pointer.parent = pointer;
+				}
+	
+			}	
+			if (down == levelDown){
+				if(ex == expand){
+					pointer.name = teams.get(t);
+					pointer.parent.right.name = teams.get(t+1);
+					pointer.parent.name = "not played";
+					pointer.parent.parent = pointer;
+					down = down -1;
+					t=t+2;
+				}
+				else {
+					pointer.left.name = teams.get(t);
+					pointer.right.name = teams.get(t+1);
+					pointer.name = "not played";
+					pointer.parent = pointer;
+					down = down -1;
+					t=t+2;
+					ex++;
+				}
+			}
+			else if (pointer.left.name != "not played"){
+				pointer.left = pointer;
+				down++;
+			}
+			else if (pointer.left.name == "not played"){
+				pointer.right = pointer;
+			}
+				
+			
+		}
+		
 		
 	}
 	
 	
 	
-	private BNode root;
 	
-	public class BNode{
-		private BNode left, right, perent;
+	public class BNode implements IBinaryTree{
+		private BNode left, right, parent;
 		private String name;
+		private int score;
+		
+		public BNode getLeft() {
+			return left;
+		}
+		
+		public void setLeft(BNode left) {
+			this.left = left;
+		}
+		
+		public BNode getRight() {
+			return right;
+		}
+		
+		public void setRight(BNode right) {
+			this.right = right;
+		}
+		
+		public BNode getParent() {
+			return parent;
+		}
+		
+		public void setParent(BNode parent) {
+			this.parent = parent;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public int getScore() {
+			return score;
+		}
+		
+		public void setScore(int score) {
+			this.score = score;
+		}
+
+		@Override
+		public String getPlayer() {
+			return name;
+		}	
 	}
 }
 
